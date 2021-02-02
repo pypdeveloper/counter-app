a27debbbdd5e3169b80e8317fbf941f0e37b2a9e
pypdeveloper <43103441+pypdeveloper@users.noreply.github.com>
2021-02-02 18:06:24 +0530

    adding the project code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1>React</h1>
+      <p>{counter}</p>
+      <button className="button" onClick={() => setCounter(counter + 1)}>
+        Increase counter
+      </button>
+      <button className="button" onClick={() => setCounter(counter - counter)}>
+        Reset counter
+      </button>
     </div>
   );
 }
